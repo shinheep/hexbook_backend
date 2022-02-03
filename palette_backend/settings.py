c886dc69f01a92282ef83f99272cee4e99f3af64
Shinhee Park <shinhee.park777@gmail.com>
2022-02-03 11:05:57 -0800

    hid secret key

--- palette_backend/settings.py
+++ palette_backend/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+from decouple import config
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xua#&)ldl)-rf9$!u=)7ode3s#^9kig!owmwv#oca7z6$7nu4g'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
