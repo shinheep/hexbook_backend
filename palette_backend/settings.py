7008a647a6ebb57cd8550dca8e609c33289b8428
Shinhee Park <shinhee.park777@gmail.com>
2022-02-03 11:13:38 -0800

    Changed Database engine to postgresql

--- palette_backend/settings.py
+++ palette_backend/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'palette_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'palette_backend',
     }
 }
