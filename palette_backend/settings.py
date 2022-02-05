9437b6d2fa32155cdc84ecc194ea5ce9e3ea1d6c
Shinhee Park <shinhee.park777@gmail.com>
2022-02-04 17:17:00 -0800

    Installed CORS header to solve cors error for frontend

--- palette_backend/settings.py
+++ palette_backend/settings.py
@@ -40,17 +40,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'palette_backend.urls'
